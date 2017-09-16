46f958b83d451ebce231100429d7cc57d69b7adf
Clark Carter <clark@clarkcarter.com>
2017-09-17 08:37:29 +1000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// Multi Step Form taken from tutorial at
+//https://www.viget.com/articles/building-a-multi-step-registration-form-with-react
+
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+      Hello, World.
+      </div>
+    )
+  }
+}
+
+export default App;
